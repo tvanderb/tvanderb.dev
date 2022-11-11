7a980c0d33c5d82265b6022571c318514ab8bed4
Talon Vanderbeken <git@knock.email>
2022-11-11 17:58:39 +0000

    www: Cleaned up pageloader.js

--- public_html/js/pageloader.js
+++ public_html/js/pageloader.js
@@ -1,16 +1,8 @@
-function PLLoadRecentPosts(elementId, resourceId) {
-    const list = document.getElementById(elementId);
-    const resource = list.getAttribute("pageloader-resource-id");
-    
-    if (!resource) {
-        console.error(`pageloader: element={${elementId}} Was not provided with a resource ID by attribute or parameter.`);
-        return;
-    }
-
-    fetch(`/resources/${resource}.json`)
+async function PLFetchResource(elementId, resourceId, callback) {
+    fetch(`/resources/${resourceId}.json`)
         .then((response) => {
             if (!response.ok) {
-                throw new Error(`pageloader: element={${elementId}} resource={${resource}} Received nonstandard HTTP status: ${response.status}`);
+                throw new Error(`pageloader: element={${elementId}} resource={${resourceId}} Received nonstandard HTTP status: ${response.status}`);
             }
 
             return response.text();
@@ -20,23 +12,35 @@ function PLLoadRecentPosts(elementId, resourceId) {
             try {
                 json = JSON.parse(data);
             } catch (error) {
-                throw new Error(`pageloader: element={${elementId}} resource={${resource}} Resource contains invalid JSON data`)
+                throw new Error(`pageloader: element={${elementId}} resource={${resourceId}} Resource contains invalid JSON data`)
             }
 
-            if (json) {
-                try {
-                    json["posts"].forEach((post) => {
-                        const li = document.createElement("li");
-                        const a = document.createElement("a");
-                        a.setAttribute("href", `/posts/${post.id}`);
-                        a.setAttribute("target", "_blank");
-                        a.text = post.date;
-
-                        li.appendChild(a);
-                        list.appendChild(li);
-                    })
-                } catch (_) {}
-            }
+            callback(json);
         })
-        .catch((error) => { throw new Error(`pageloader: element={${elementId}} resource={${resource}} An error occurred during resource request.`)})
+        .catch((_) => { throw new Error(`pageloader: element={${elementId}} resource={${resourceId}} An error occurred during resource request.`)});
+}
+
+function PLLoadRecentPosts(elementId, resourceId) {
+    const list = document.getElementById(elementId);
+    const resource = list.getAttribute("pageloader-resource-id");
+    
+    if (!resource) {
+        console.error(`pageloader: element={${elementId}} Was not provided with a resource ID by attribute or parameter.`);
+        return;
+    }
+
+    PLFetchResource(elementId, resource, (json) => {
+        try {
+            json["posts"].forEach((post) => {
+                const li = document.createElement("li");
+                const a = document.createElement("a");
+                a.setAttribute("href", `/posts/${post.id}`);
+                a.setAttribute("target", "_blank");
+                a.text = post.date;
+
+                li.appendChild(a);
+                list.appendChild(li);
+            })
+        } catch (_) {}
+    });
 }
